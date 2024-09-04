bce586f28f6293f7f3a846500d5729a1c3c4a652
Alexander Serebryakov <alex.serebr@gmail.com>
2024-09-04 14:25:18 +0300

    Changed algorithm to scheduled execution, but there's no blinking green at the moment.

--- traffic_light/traffic_light.ino
+++ traffic_light/traffic_light.ino
@@ -1,64 +1,53 @@
-/*
-  Fade
-
-  This example shows how to fade an LED on pin 9 using the analogWrite()
-  function.
-
-  The analogWrite() function uses PWM, so if you want to change the pin you're
-  using, be sure to use another PWM capable pin. On most Arduino, the PWM pins
-  are identified with a "~" sign,                              ~3, ~5, ~6, ~9, ~10 and ~11.
-
-  This example code is in the public domain.
-
-  https://www.arduino.cc/en/Tutorial/BuiltInExamples/Fade
-*/
-
+/**
+ * Simple traffic light control.
+ */
 const int RED_LED = 9;
 const int YELLOW_LED = 10;
 const int GREEN_LED = 11;
-int TIME = 1000;
 const int DELAY_PIN = A0;
-
 const int BRIGHTNESS = 64;  // how bright the LED is
 
-// the setup routine runs once when you press reset:
-void setup() {
-  // declare pin 9 to be an output:
-  pinMode(RED_LED, OUTPUT);
-  pinMode(YELLOW_LED, OUTPUT);
-  pinMode(GREEN_LED, OUTPUT);
-  // pinMode(DELAY_PIN, OUTPUT);
-  Serial.begin(9600);
-}
+const int BUTTON_PIN{4};
 
-void reset() {
-  TIME = analogRead(DELAY_PIN) * 3;
-  Serial.println(TIME);
+int TIME = 1000;
+
+class TrafficLightColorSetter {
+  public:
+    static void green();
+    static void green_blinking();
+    static void yellow();
+    static void red();
+    static void red_yellow();
+  
+  private:
+    static void reset();
+};
+
+using TrafficLightColorSet = void (*)();
+
+void TrafficLightColorSetter::reset() {
   analogWrite(RED_LED, 0);
   analogWrite(YELLOW_LED, 0);
   analogWrite(GREEN_LED, 0);
 }
 
-void red() {
+void TrafficLightColorSetter::red() {
   reset();
   analogWrite(RED_LED, BRIGHTNESS);
-  delay(TIME);
 }
 
-void red_yellow() {
+void TrafficLightColorSetter::red_yellow() {
   reset();
   analogWrite(RED_LED, BRIGHTNESS);
   analogWrite(YELLOW_LED, BRIGHTNESS);
-  delay(TIME);
 }
 
-void green() {
+void TrafficLightColorSetter::green() {
   reset();
   analogWrite(GREEN_LED, BRIGHTNESS / 4);
-  delay(TIME);
 }
 
-void green_blinking() {
+void TrafficLightColorSetter::green_blinking() {
   for (int i = 0; i < 4; i++) {
     reset();
     delay(TIME / 4);
@@ -67,17 +56,103 @@ void green_blinking() {
   }
 }
 
-void yellow() {
+void TrafficLightColorSetter::yellow() {
   reset();
   analogWrite(YELLOW_LED, BRIGHTNESS);
-  delay(TIME);
 }
 
-// the loop routine runs over and over again forever:`
+class TrafficLightControl {
+  public:
+    void loop_callback();
+
+  private:
+    int read_interval() const;
+    bool button_pressed() const;
+    void change_state();
+
+    static constexpr int GREEN{8};
+    static constexpr int GREEN_BLINKING{4};
+    static constexpr int YELLOW{3};
+    static constexpr int RED{11};
+    static constexpr int RED_YELLOW{1};
+    // static constexpr int RESTART{RED_YELLOW + 1};
+
+    static int states[];
+    static TrafficLightColorSet states_handlers[];
+
+    int current_msecs{0};
+    size_t current_state{0};
+    int time_multiplier{500};
+};
+
+int TrafficLightControl::states[] = {GREEN, /*GREEN_BLINKING,*/ YELLOW, RED, RED_YELLOW};
+
+TrafficLightColorSet TrafficLightControl::states_handlers[] = {
+  TrafficLightColorSetter::green,
+  // TrafficLightColorSetter::green_blinking,
+  TrafficLightColorSetter::yellow,
+  TrafficLightColorSetter::red,
+  TrafficLightColorSetter::red_yellow
+  };
+
+// static_assert(sizeof(states) == sizeof(states_handlers));
+
+void TrafficLightControl::change_state() {
+  Serial.println(current_state);
+
+  current_state = current_state + 1;
+
+  if (current_state >= sizeof(states)/sizeof(int)) {
+    Serial.println("Reseting state");
+    current_state = 0;
+  }
+
+  // Serial.print("Current state: ");
+  Serial.println(current_state);
+  (*states_handlers[current_state])();
+}
+
+void TrafficLightControl::loop_callback() {
+  constexpr int INTERVAL{100};
+  current_msecs += INTERVAL;
+
+  if (current_msecs > states[current_state] * time_multiplier) {
+    Serial.println(current_msecs);
+    change_state();
+    current_msecs = 0;
+  }
+
+  if (button_pressed() && current_state == 0) {
+    change_state();
+    current_msecs = 0;
+  }
+
+  delay(INTERVAL);
+}
+
+int TrafficLightControl::read_interval() const {
+  return analogRead((DELAY_PIN * 1000) / 1024);
+}
+
+bool TrafficLightControl::button_pressed() const {
+  return digitalRead(BUTTON_PIN) == 0;
+}
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // declare pin 9 to be an output:
+  pinMode(RED_LED, OUTPUT);
+  pinMode(YELLOW_LED, OUTPUT);
+  pinMode(GREEN_LED, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  Serial.begin(57600);
+
+  TrafficLightColorSetter::green(); // Just to setup initial state
+  Serial.println(42); // If you see this in the log more than once - code just crashed =)
+}
+
+TrafficLightControl control{};
+
 void loop() {
-  red();
-  red_yellow();
-  green();
-  green_blinking();
-  yellow();
+  control.loop_callback();
 }

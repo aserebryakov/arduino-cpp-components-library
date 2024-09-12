6e5af64a49dfe4468ecb41b1bac9fb1703492dd5
Alexander Serebryakov <alex.serebr@gmail.com>
2024-09-12 09:46:19 +0300

    Adding support library for projects

---
+++ support_library/Scheduler.cpp
@@ -0,0 +1,5 @@
+//
+// Created by alex on 9/12/24.
+//
+
+#include "Scheduler.h"
---
+++ support_library/Scheduler.h
@@ -0,0 +1,16 @@
+//
+// Created by alex on 9/12/24.
+//
+
+#ifndef SCHEDULER_H
+#define SCHEDULER_H
+
+
+
+class Scheduler {
+
+};
+
+
+
+#endif //SCHEDULER_H

97eb9e9d0a49f68b8a22df5111f2b6c2ae71ca1a
Alexander Serebryakov <alex.serebr@gmail.com>
2024-10-04 15:54:44 +0300

    DcsControl: Encoder prototype.

--- dcs_control/dcs_control.ino
+++ dcs_control/dcs_control.ino
@@ -4,56 +4,70 @@
 
 const int pinLed = LED_BUILTIN;
 const int pinButton = 2;
-constexpr int DT_PIN{4};
-constexpr int CLK_PIN{5};
-constexpr int SW_PIN{3};
+constexpr int DT_PIN{ 4 };
+constexpr int CLK_PIN{ 5 };
+constexpr int SW_PIN{ 3 };
 
 HwApiImpl hw_api{};
-RotaryEncoder encoder{DT_PIN, CLK_PIN, SW_PIN, hw_api};
+RotaryEncoder encoder{ DT_PIN, CLK_PIN, SW_PIN, hw_api };
 
 
 void onTurnRight(void*) {
+  Gamepad.press(1);
+  Gamepad.write();
+  Gamepad.releaseAll();
+  Gamepad.write();
+
   Serial.println("Right");
-    Serial.println(digitalRead(SW_PIN));
+  Serial.println(digitalRead(SW_PIN));
   Serial.println(digitalRead(DT_PIN));
   Serial.println(digitalRead(CLK_PIN));
-
 }
 
 void onTurnLeft(void*) {
+  Gamepad.press(2);
+  Gamepad.write();
+  Gamepad.releaseAll();
+  Gamepad.write();
+
+
   Serial.println("Left");
-    Serial.println(digitalRead(SW_PIN));
+  Serial.println(digitalRead(SW_PIN));
   Serial.println(digitalRead(DT_PIN));
   Serial.println(digitalRead(CLK_PIN));
-
 }
 
 void onClick(void*) {
+  Gamepad.press(3);
+  Gamepad.write();
+  Gamepad.releaseAll();
+  Gamepad.write();
+
+
   Serial.println("Click");
-    Serial.println(digitalRead(SW_PIN));
+  Serial.println(digitalRead(SW_PIN));
   Serial.println(digitalRead(DT_PIN));
   Serial.println(digitalRead(CLK_PIN));
-
 }
 
 void setup() {
   pinMode(pinLed, OUTPUT);
   pinMode(pinButton, INPUT_PULLUP);
-  // pinMode(DT_PIN, INPUT);
-  // pinMode(CLK_PIN, INPUT);
-  // pinMode(SW_PIN, INPUT);
+  pinMode(DT_PIN, INPUT);
+  pinMode(CLK_PIN, INPUT);
+  pinMode(SW_PIN, INPUT);
 
-    pinMode(DT_PIN, INPUT_PULLUP);
-  pinMode(CLK_PIN, INPUT_PULLUP);
-  pinMode(SW_PIN, INPUT_PULLUP);
+  //   pinMode(DT_PIN, INPUT_PULLUP);
+  // pinMode(CLK_PIN, INPUT_PULLUP);
+  // pinMode(SW_PIN, INPUT_PULLUP);
 
   // Sends a clean report to the host. This is important on any Arduino type.
   Gamepad.begin();
   Serial.begin(9600);
 
-  encoder.setPushButtonCallback({onClick, nullptr});
-  encoder.setTurnClockwiseCallback({onTurnRight, nullptr});
-  encoder.setTurnCounterClockwiseCallback({onTurnLeft, nullptr});
+  encoder.setPushButtonCallback({ onClick, nullptr });
+  encoder.setTurnClockwiseCallback({ onTurnRight, nullptr });
+  encoder.setTurnCounterClockwiseCallback({ onTurnLeft, nullptr });
 }
 
 void loop() {
@@ -63,6 +77,7 @@ void loop() {
   // Serial.println(digitalRead(SW_PIN));
   // Serial.println(digitalRead(DT_PIN));
   // Serial.println(digitalRead(CLK_PIN));
+  // Serial.println(digitalRead(pinButton));
 
 
   if (!digitalRead(pinButton)) {
@@ -74,8 +89,7 @@ void loop() {
     if (count == 33) {
       Gamepad.releaseAll();
       count = 0;
-    }
-    else
+    } else
       Gamepad.press(count);
 
     // Move x/y Axis to a new position (16bit)

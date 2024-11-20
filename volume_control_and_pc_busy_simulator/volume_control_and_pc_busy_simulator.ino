8b2c48df37cbb4e9aed391ac626734516a6d3189
Alexander Serebryakov <alex.serebr@gmail.com>
2024-11-20 18:56:44 +0200

    VolumeControl: Adopted MouseControl to use new infrastructure.

--- volume_control_and_pc_busy_simulator/volume_control_and_pc_busy_simulator.ino
+++ volume_control_and_pc_busy_simulator/volume_control_and_pc_busy_simulator.ino
@@ -14,109 +14,139 @@ const int MOUSE_SWITCH_PIN = 20;
 const int MOUSE_LED_PIN = 21;
 const int KEYBOARD_SWITCH_PIN = 19;
 const int KEYBOARD_LED_PIN = 10;
+constexpr int TICK{5};
 
 HwApiImpl hw_api{};
-
 
 class VolumeControl {
 public:
   void setup() {
     Consumer.begin();
-    control.setup();
   }
 
   void loop() {
-    control.loop();
   }
 
-private:
-    GenericController<Control, 1> control{
-      HeapObject<Control>(new RotaryEncoder{
-        {DT_PIN, HwApi::PIN_MODE::INPUT_PULLUP_MODE, hw_api},
-        {CLK_PIN, HwApi::PIN_MODE::INPUT_PULLUP_MODE, hw_api},
-        {SW_PIN, HwApi::PIN_MODE::INPUT_PULLUP_MODE, hw_api},
-        {onTurnClockwise, nullptr},
-        {onTurnCounterClockwise, nullptr},
-        {onPushButton, nullptr}
-        })};
-
-  static void onTurnClockwise(void*) {
+  static void volumeUp(void*) {
     Consumer.write(MEDIA_VOL_UP);
     Consumer.write(MEDIA_VOL_UP);
   }
 
-  static void onTurnCounterClockwise(void*) {
+  static void volumeDown(void*) {
     Consumer.write(MEDIA_VOL_DOWN);
     Consumer.write(MEDIA_VOL_DOWN);
   }
 
-  static void onPushButton(void*) {
+  static void mute(void*) {
     Consumer.write(MEDIA_VOL_MUTE);
   }
 };
 
-VolumeControl volume_control{};
+
+class MouseControl {
+public:
+  MouseControl(Scheduler& scheduler, HwApi& hw_api) : scheduler{scheduler}, hw_api{hw_api} {
+  }
+
+  void setup() {
+    hw_api.pinMode(MOUSE_LED_PIN, HwApi::PIN_MODE::OUTPUT_MODE);
+    Mouse.begin();
+  }
+
+  static void mouseTask(void* self) {
+    static_cast<MouseControl*>(self)->move();
+  }
+
+  static void onSwitch(void* self) {
+    static_cast<MouseControl*>(self)->onSwitch();
+  }
+
+  void onSwitch() {
+    if (!enabled) {
+      task_id = scheduler.addPeriodicTask({mouseTask, this}, 20);
+      enabled = true;
+      hw_api.digitalWrite(MOUSE_LED_PIN, 1);
+      return;
+    }
+
+    scheduler.removeTask(task_id);
+    hw_api.digitalWrite(MOUSE_LED_PIN, 0);
+    enabled = false;
+  }
+
+  void move() {
+    Mouse.move(x_increment, y_increment);
+    moves_made++;
+    if (moves_made % 60 == 0) {
+      x_increment = -x_increment;
+      y_increment = -y_increment;
+      Mouse.click();
+    }
+  }
+
+private:
+  Scheduler& scheduler;
+  HwApi& hw_api;
+  bool enabled{false};
+  SchedulerTaskId task_id{0};
+  int moves_made{0};
+  int x_increment{3};
+  int y_increment{3};
+};
+
+class Controller {
+public:
+  void setup() {
+    control.setup();
+    volume_control.setup();
+    mouse_control.setup();
+  }
+
+  void loop() {
+    control.loop();
+    volume_control.loop();
+    scheduler.tick(TICK);
+    delay(TICK);
+  }
+
+private:
+    Scheduler scheduler{};
+    VolumeControl volume_control{};
+    MouseControl mouse_control{scheduler, hw_api};
+
+    GenericController<Control, 2> control{
+      HeapObject<Control>(new RotaryEncoder{
+        {DT_PIN, HwApi::PIN_MODE::INPUT_PULLUP_MODE, hw_api},
+        {CLK_PIN, HwApi::PIN_MODE::INPUT_PULLUP_MODE, hw_api},
+        {SW_PIN, HwApi::PIN_MODE::INPUT_PULLUP_MODE, hw_api},
+        {VolumeControl::volumeUp, nullptr},
+        {VolumeControl::volumeDown, nullptr},
+        {VolumeControl::mute, nullptr}
+        }),
+      HeapObject<Control>(new DigitalPin{
+        MOUSE_SWITCH_PIN,
+        HwApi::PIN_MODE::INPUT_PULLUP_MODE,
+        {MouseControl::onSwitch, &mouse_control},
+        {},
+        hw_api
+        })};
+};
+
+Controller controller{};
 
 void setup() {
-  // pinMode(MOUSE_SWITCH_PIN, INPUT_PULLUP);
-  // pinMode(MOUSE_LED_PIN, OUTPUT);
   // pinMode(KEYBOARD_SWITCH_PIN, INPUT_PULLUP);
   // pinMode(KEYBOARD_LED_PIN, OUTPUT);
 
-  // Consumer.begin();
-  // Mouse.begin();
-  volume_control.setup();
+  controller.setup();
   Serial.begin(9600);
   Serial.println(42);
 }
 
 void loop() {
-  volume_control.loop();
-  delay(5);
+  controller.loop();
 }
 
-
-// class MouseControl {
-// public:
-//   MouseControl(Scheduler& scheduler, HwApi& hw_api) : scheduler{scheduler}, hw_api{hw_api} {
-//   }
-
-//   static void mouseTask(void* self) {
-//     static_cast<MouseControl*>(self)->move();
-//   }
-
-//   void onSwitch() {
-//     if (!enabled) {
-//       task_id = scheduler.addPeriodicTask({mouseTask, this}, 20);
-//       enabled = true;
-//       hw_api.digitalWrite(MOUSE_LED_PIN, 1);
-//       return;
-//     }
-
-//     scheduler.removeTask(task_id);
-//     hw_api.digitalWrite(MOUSE_LED_PIN, 0);
-//     enabled = false;
-//   }
-
-//   void move() {
-//     Mouse.move(x_increment, y_increment);
-//     moves_made++;
-//     if (moves_made % 60 == 0) {
-//       x_increment = -x_increment;
-//       y_increment = -y_increment;
-//       Mouse.click();
-//     }
-//   }
-
-// private:
-//   Scheduler& scheduler;
-//   HwApi& hw_api;
-//   bool enabled{false};
-//   SchedulerTaskId task_id{0};
-//   int moves_made{0};
-//   int x_increment{3};
-//   int y_increment{3};
-// };
 
 // class KeyboardControl {
 // public:
